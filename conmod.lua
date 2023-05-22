253377da9308629c0b46867eab8d73efe687301a
Apethesis <apethesis@gmail.com>
2023-05-23 00:46:21 +0200

    Added randomizer

--- conmod.lua
+++ conmod.lua
@@ -1,7 +1,26 @@
 _G.shitsweird = false
-local changed = false
+local chaos = false
+local truechaos = false
+local changable = {
+    [1] = "Color",
+    [2] = "Falldown",
+    [3] = "AirDrag",
+    [4] = "Flammable",
+    [5] = "Hardness",
+    [6] = "Temperature",
+    [7] = "State",
+    [8] = "HotAir",
+    [9] = "Diffusion",
+    [10] = "AirLoss",
+    [11] = "Loss",
+    [12] = "Collision",
+    [13] = "Gravity",
+    [14] = "Advection",
+    [15] = "HeatConduct",
+    [16] = "Meltable"
+}
 evt.register(evt.tick,function()
-    if _G.shitsweird and changed ~= true then
+    if _G.shitsweird and chaos ~= true then
         for k,v in pairs(elem) do
             if type(v) ~= "function" and k:sub(1,7) == "DEFAULT" and elements.property(v,"Name") ~= "DMND" then
                 local flam = elements.property(v,"Flammable")
@@ -21,16 +40,71 @@ evt.register(evt.tick,function()
                 elements.property(v,"Explosive",2)
             end
         end
-        changed = true
-    elseif _G.shitsweird == false and changed ~= false then
+        print("finished regular chaos")
+        chaos = true
+    elseif _G.shitsweird == false and chaos ~= false then
         for k,v in pairs(elem) do
             if type(v) ~= "function" and k:sub(1,7) == "DEFAULT" and elements.property(v,"Name") ~= "DMND" then
                 elements.loadDefault(v)
             end
         end
-        changed = false
+        chaos = false
+        print("disabled regular chaos")
+    elseif _G.shitscrazy and truechaos ~= true then
+        for k,v in pairs(elem) do
+            if type(v) ~= "function" and k:sub(1,7) == "DEFAULT" and elements.property(v,"Name") ~= "DMND" then
+                local flam = elements.property(v,"Flammable")
+                local fall = elements.property(v,"Falldown")
+                if flam > 0 then
+                    elements.property(v,"Flammable",0)
+                else
+                    elements.property(v,"Flammable",1000)
+                end
+                if fall == 0 then
+                    elements.property(v,"Falldown",1)
+                elseif fall == 1 then
+                    elements.property(v,"Falldown",2)
+                elseif fall == 2 then
+                    elements.property(v,"Falldown",0)
+                end
+                elements.property(v,"Explosive",2)
+            end
+        end
+        print("finished regular chaos")
+        print("randomizer will take a while, if it asks you to stop a 'loop' just cancel")
+        for k,v in pairs(elem) do
+            if type(v) ~= "function" and k:sub(1,7) == "DEFAULT" and elements.property(v,"Name") ~= "DMND" then
+                for k1,v1 in ipairs(changable) do
+                    if v1 == "Color" or v1 == "Temperature" then
+                        elements.property(v,v1,math.random(1,16777215))
+                    elseif v1 == "AirLoss" or v1 == "Collision" or v1 == "Advection" or v1 == "AirDrag" or v1 == "HotAir" or v1 == "Loss" then
+                        elements.property(v,v1,math.random(0,255)/255)
+                    elseif v1 == "Hardness" or v1 == "HeatConduct" or v1 == "Meltable" then
+                        elements.property(v,v1,math.random(0,255))
+                    elseif v1 == "Gravity" or v1 == "HotAir" then
+                        elements.property(v,v1,math.random(-100,100))
+                    elseif v1 == "Weight" then
+                        elements.property(v,v1,math.random(-1,100))
+                    elseif v1 == "Falldown" then
+                        elements.property(v,v1,math.random(0,2))
+                    elseif v1 == "State" then
+                        elements.property(v,v1,math.random(0,4))
+                    end
+                end
+            end
+        end
+        print("finished randomizer")
+        truechaos = true
+        print("finished crazying shit up (true chaos enabled)")
+    elseif _G.shitscrazy == false and truechaos ~= false then
+        for k,v in pairs(elem) do
+            if type(v) ~= "function" and k:sub(1,7) == "DEFAULT" then
+                elements.loadDefault(v)
+            end
+        end
+        truechaos = false
+        print("disabled true chaos")
     end
-
 end)
 for k,v in pairs(elem) do
     if type(v) ~= "function" and k:sub(1,7) == "DEFAULT" and elements.property(v,"Name") ~= "DMND" then
